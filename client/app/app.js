67fbf183bdee10865239061e8bf4fbeef9daef39
Mario Roaf-Esparza <marioroafesparza@gmail.com>
2016-07-27 19:23:36 -0700

    use hidden keys for firebase

--- client/app/app.js
+++ client/app/app.js
@@ -11,18 +11,18 @@ angular.module('gl', ['gl.list', 'ngRoute', 'firebase'])
 .factory("List", ["$firebaseArray",
   function($firebaseArray) {
 
-  	//connect to db
-	var config = {
-	  apiKey: "AIzaSyD2Mhdg1YKT1WvUUGzJgQm_JklUKGF1nDg",
-	  authDomain: "project-3889533569816587013.firebaseapp.com",
-	  databaseURL: "https://project-3889533569816587013.firebaseio.com",
-	  storageBucket: "project-3889533569816587013.appspot.com",
-	};
+	//connect to db
+    var config = {
+      apiKey: process.env.APIKEY,
+      authDomain: process.env.AUTHDOMAIN,
+      databaseURL: process.env.DBURL,
+      storageBucket: process.env.STORAGEBUCKET
+    };
 	// firebase.initializeApp(config);
 	// var db = firebase.database();
 	// var ref = db.ref("/");
 	// console.log(List, 'list');
-	var ref = new Firebase("https://project-3889533569816587013.firebaseio.com");
+	var ref = new Firebase(config.databaseURL);
 	var List = ref.child('list');
 
     return $firebaseArray(List);
